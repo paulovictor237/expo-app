4755fef6216928aed94d5292c4b933a83c197f3a
Paulo Victor <paulovictor237@gmail.com>
2023-06-15 20:58:49 -0300

    absolute path

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      require.resolve("expo-router/babel"),
+      require.resolve('expo-router/babel'),
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: { '@': './src' },
+          extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        },
+      ],
     ],
   };
 };
