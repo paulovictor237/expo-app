f6b76865bd870cc75a3dd14e096e28ab791d7dd4
Paulo Victor <paulovictor237@gmail.com>
2023-06-16 07:56:31 -0300

    new libs

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,8 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
       'nativewind/babel',
       require.resolve('expo-router/babel'),
       [
